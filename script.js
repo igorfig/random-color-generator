7f0f1bca1f47e89c360df7c346b52c100c8ee02d
Igor Figueiredo Silva <igorfigueiredors@gmail.com>
2021-04-26 14:08:40 -0300

    improved access functions to the previous and next values

--- script.js
+++ script.js
@@ -1,89 +1,88 @@
-const hex = document.querySelector("#hex")
-const generate = document.querySelector(".generate")
-const copy = document.querySelector(".copy")
+const hex = document.querySelector("#hex");
+const generate = document.querySelector(".generate");
+const copy = document.querySelector(".copy");
 
 /* style */
 
-const header = document.querySelector('header')
-const button = document.querySelector("button")
+const header = document.querySelector("header");
+const button = document.querySelector("button");
 
 /* ================== */
 
 /* previous & next */
-
-const previousArrow = document.querySelector(".previous")
-const nextArrow =  document.querySelector(".next")
+const previousArrow = document.querySelector(".previous");
+const nextArrow = document.querySelector(".next");
 
 let values = [];
-let next = [];
 let previous = [];
-
+let counter;
 /* ================== */
 
 function randomColor() {
-    const hex = (Math.random() * 0XFFFFFF << 0).toString(16);
+  const hex = ((Math.random() * 0xffffff) << 0).toString(16);
+  if (hex.length >= 6) {
     return `#${hex}`;
+  } else {
+    return `#${hex}` + Math.floor(Math.random() * (5 + 1) + 3);
+  }
 }
 
 function addColors() {
-    hex.value = randomColor()
-    header.style.backgroundColor = `${hex.value}`
-    button.style.backgroundColor = `${hex.value}`
-    
-    next.push(hex.value)
+  hex.value = randomColor();
+  header.style.backgroundColor = `${hex.value}`;
+  button.style.backgroundColor = `${hex.value}`;
 
-    if(next.length > 1) {
-        next.shift()
-    }
+  values.push(hex.value);
+  counter = values.length - 1;
 
-    values.push(hex.value)
+  nextArrow.classList.add("hide-arrow");
 
-    if (values.length > 2) {
-        values.splice(0, 1)
-    }
+  const index = values.length - 1;
 
-    previous = values.slice(-2, -1)
+  previous = values.slice(0, index);
 
-    if (previous.length > 0) {
-        previousArrow.classList.remove('hide-arrow')
-    }
-        
-    if(!nextArrow.classList.contains('hide-arrow')){
-        nextArrow.classList.add('hide-arrow')
-    }
+  if (previous.length > 0) {
+    previousArrow.classList.remove("hide-arrow");
+  }
 
-    hex.classList.remove('hide');
-    copy.classList.remove('hide');
-
+  hex.classList.remove("hide");
+  copy.classList.remove("hide");
 }
 
 const Arrows = {
-    getNextValue(){    
-        hex.value = next
-        header.style.backgroundColor = `${next}`
-        button.style.backgroundColor = `${next}`
+  getNextValue() {
+    previousArrow.classList.remove("hide-arrow");
+    ++counter;
+    hex.value = values[counter];
+    header.style.backgroundColor = `${hex.value}`;
+    button.style.backgroundColor = `${hex.value}`;
 
-        nextArrow.classList.add('hide-arrow')
-        previousArrow.classList.remove('hide-arrow')
-    },
+    if (counter == values.length - 1) {
+      nextArrow.classList.add("hide-arrow");
+    }
+  },
 
-    getPreviousValue(){
-        hex.value = previous
-        header.style.backgroundColor = `${previous}`
-        button.style.backgroundColor = `${previous}`
+  getPreviousValue() {
+    if (counter > 0) {
+      --counter;
+    } else {
+      previousArrow.classList.add("hide-arrow");
+    }
 
-        previousArrow.classList.add('hide-arrow')
-        nextArrow.classList.remove('hide-arrow')
+    hex.value = previous[counter];
+    header.style.backgroundColor = `${hex.value}`;
+    button.style.backgroundColor = `${hex.value}`;
 
-    }   
-}
+    nextArrow.classList.remove("hide-arrow");
+  },
+};
 
-nextArrow.addEventListener('click', Arrows.getNextValue)
-previousArrow.addEventListener('click', Arrows.getPreviousValue)
+nextArrow.addEventListener("click", Arrows.getNextValue);
+previousArrow.addEventListener("click", Arrows.getPreviousValue);
 
-generate.addEventListener('click', addColors)
+generate.addEventListener("click", addColors);
 
-copy.addEventListener('click', () => {
-    hex.select();
-    document.execCommand('copy')
-})
+copy.addEventListener("click", () => {
+  hex.select();
+  document.execCommand("copy");
+});
